c015766ff391d5b74f8a1aae841c4037a1100533
박진성 <plashdof@gmail.com>
2022-12-30 00:58:38 +0900

    a

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express =require('express');
 const app = express();
 
+app.get('/', (req,res)=>{
+    res.send('Hello World');
+})
+
+app.get('/api/courses', (req,res)=>{
+    res.send([1,2,3]);
+})
+
+
 app.listen(8080,function(){
     console.log('listening on 8080');
 })
