9625fc07f9d281bd8d317d49c9bd6d1c342ee2fc
박진성 <plashdof@gmail.com>
2023-01-13 03:24:24 +0900

    sdf

--- server.js
+++ server.js
@@ -1,77 +1,42 @@
-const express =require('express');
-const mysql = require('mysql')
-var bodyParser = require('body-parser');
+const express = require('express');
 const app = express();
+var SerialPort = require("serialport");
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+var port = 3000;
 
-// aws RDS 와 연결
-var connection = mysql.createConnection({
-    host: "testdb.cvnx8yjnprze.ap-northeast-2.rds.amazonaws.com",
-    user: "plashdof",
-    database: "testdb",
-    password:"jt8026jt97!",
-    port: 3306
+var arduinoCOMPort = "COM3";
+
+var arduinoSerialPort = new SerialPort(arduinoCOMPort, {  
+ baudrate: 9600
+});
+
+arduinoSerialPort.on('open',function() {
+  console.log('Serial Port ' + arduinoCOMPort + ' is opened.');
+});
+
+app.get('/', function (req, res) {
+
+    return res.send('Working');
+ 
 })
 
-// 서버 테스트용 get API
-app.get('/', (req,res)=>{
-    const result = {
-        userId : 1234,
-        userPwd : "1234"
+app.get('/:action', function (req, res) {
+    
+   var action = req.params.action || req.param('action');
+    
+    if(action == 'led'){
+        arduinoSerialPort.write("w");
+        return res.send('Led light is on!');
+    } 
+    if(action == 'off') {
+        arduinoSerialPort.write("t");
+        return res.send("Led light is off!");
     }
-    res.send(result);
-})
+    
+    return res.send('Action: ' + action);
+ 
+});
 
-
-// 로그인 post API
-app.post('/login', (req,res)=>{ 
-
-    // 프론트에서 body 받아서 userPwd 와 userId 변수에 넣기
-    var userPwd = req.body.userPwd;
-    var userId = req.body.userId;
-
-    // 존재하는 ID 인지 데이터베이스에서 확인하는 Query문
-    var sql = 'select * from Users where UserId = ?';
-
-    // Query문 데이터베이스에서 돌리기
-    connection.query(sql, userId, (err,result)=>{
-        var resultCode = 404;
-        var message = 'error';
-        
-        // Query문 에러체크
-        if(err){
-            
-        }else{    
-
-            if(result.length === 0){
-
-                // ID가 DB에 없을경우
-                resultCode = 204;
-                message = '존재하지 않는 계정';
-            }else if(userPwd !== result[0].UserPwd){
-
-                // ID는 있지만, 비밀번호가 다를경우
-                resultCode = 204;
-                message = '비밀번호가 틀렸습니다';
-            }else{
-                // ID 와 비밀번호 모두 일치할 경우
-                resultCode = 200;
-                message = '로그인 성공!!' + result[0].UserName + '님 환영합니다!';
-            }
-        }
-
-        // 프론트로 보내는 응답코드, 메세지
-        res.json({
-            'code': resultCode,
-            'message':message 
-        })
-
-    })
-})
-
-
-app.listen(8080,function(){
-    console.log('listening on 8080');
-})
+app.listen(port, function () {
+  console.log('Example app listening on port http://0.0.0.0:' + port + '!');
+});
